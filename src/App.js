fbc2cdcbcad3b142943854c4e9d4e40aad37d957
leostricak22 <stricak.leo@gmail.com>
2024-09-15 15:56:13 +0200

    feat: Add timer based modal close and progress bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import {useRef, useState, useEffect, useCallback} from "react";
 
 import Places from "./components/Places";
 import { AVAILABLE_PLACES } from "./data";
@@ -47,7 +47,7 @@ function App() {
       localStorage.setItem("selectedPlaces", JSON.stringify([...storedIds, id]));
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(() => {
     setPickedPlaces((prevPickedPlaces) =>
         prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -56,15 +56,17 @@ function App() {
 
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     localStorage.setItem("selectedPlaces", JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current)));
-  }
+  }, []);
 
   return (
       <>
         <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-          <DeleteConfirmation
-              onCancel={handleStopRemovePlace}
-              onConfirm={handleRemovePlace}
-          />
+          { modalIsOpen &&
+            <DeleteConfirmation
+                onCancel={handleStopRemovePlace}
+                onConfirm={handleRemovePlace}
+            />
+          }
         </Modal>
 
         <header>
